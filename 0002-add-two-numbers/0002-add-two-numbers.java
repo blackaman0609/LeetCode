eee0127f4d24315c1ea35c66400fcda8c3d9e5b5
Aman Kumar <149137677+blackaman0609@users.noreply.github.com>
2024-10-04 22:39:02 +0530

    Time: 1 ms (100%), Space: 43.9 MB (98.33%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -8,7 +8,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next; }
  * }
  */
- 
+
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(0); // Create a dummy head for the result list
