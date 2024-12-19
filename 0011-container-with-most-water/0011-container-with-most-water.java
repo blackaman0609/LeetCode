2ed929d3951907e6466a7470a84bdfebdcaf9dcd
Aman Kumar <149137677+blackaman0609@users.noreply.github.com>
2024-12-19 15:07:28 +0530

    Time: 6 ms (7.65%), Space: 58.3 MB (16.68%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -4,17 +4,13 @@ class Solution {
         int right = height.length - 1;
         int maxArea = 0;
         
-        // Two pointer approach
         while (left < right) {
-            // Calculate the area
             int width = right - left;
             int minHeight = Math.min(height[left], height[right]);
             int area = width * minHeight;
             
-            // Update maxArea if the current area is larger
             maxArea = Math.max(maxArea, area);
             
-            // Move the pointer pointing to the shorter line
             if (height[left] < height[right]) {
                 left++;
             } else {
