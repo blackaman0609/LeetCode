0c755bd5be33400103421f4a18f4f8eab15194fe
Aman Kumar <149137677+blackaman0609@users.noreply.github.com>
2024-12-27 09:51:22 +0530

    Time: 0 ms (100%), Space: 41.5 MB (38.65%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -1,30 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    int sum=0;
     public int sumOfLeftLeaves(TreeNode root) {
-        if (root == null) {
+        if(root.left==null && root.right==null){
             return 0;
         }
-        
-        Queue<Pair<TreeNode, Boolean>> queue = new LinkedList<>();
-        queue.offer(new Pair<>(root, false));  // (node, is_left)
-        int totalSum = 0;
-        
-        while (!queue.isEmpty()) {
-            Pair<TreeNode, Boolean> pair = queue.poll();
-            TreeNode node = pair.getKey();
-            boolean isLeft = pair.getValue();
-            
-            if (isLeft && node.left == null && node.right == null) {
-                totalSum += node.val;
-            }
-            
-            if (node.left != null) {
-                queue.offer(new Pair<>(node.left, true));
-            }
-            if (node.right != null) {
-                queue.offer(new Pair<>(node.right, false));
-            }
+        helper(root);
+        return sum;
+    }
+    void helper(TreeNode root){
+        if(root==null){
+            return ;
         }
-        
-        return totalSum;
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            sum += root.left.val; 
+        }     
+        helper(root.left);
+        helper(root.right);
     }
 }
