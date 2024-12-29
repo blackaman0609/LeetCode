4772e20614d03ca138d8ac387a9a025afcf618a5
Aman Kumar <149137677+blackaman0609@users.noreply.github.com>
2024-12-30 00:38:32 +0530

    Time: 1 ms (65.06%), Space: 44.8 MB (61.61%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,17 +1,45 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        if (nums.length == 0) return 0;
+    public int removeDuplicates(int[] a) {
 
-        int k = 1; // Since the first element is always unique
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i - 1]) {
-                nums[k] = nums[i];
-                k++;
+        // if (nums.length == 0) return 0;
+
+        // int k = 1; // Since the first element is always unique
+        // for (int i = 1; i < nums.length; i++) {
+        //     if (nums[i] != nums[i - 1]) {
+        //         nums[k] = nums[i];
+        //         k++;
+        //     }
+        // }
+        // return k;
+
+        int j = 1;
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            if (a[i] < a[i + 1]) {
+                a[j] = a[i + 1];
+                j++;
             }
         }
-        return k;
+        return j;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
