51a93880b000cea35eff33dfddeb767d529c3ecb
Aman Kumar <149137677+blackaman0609@users.noreply.github.com>
2024-12-30 00:42:04 +0530

    Time: 1 ms (65.06%), Space: 44.9 MB (28.17%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,26 +1,26 @@
 class Solution {
-    public int removeDuplicates(int[] a) {
+    public int removeDuplicates(int[] nums) {
 
-        // if (nums.length == 0) return 0;
+        if (nums.length == 0) return 0;
 
-        // int k = 1; // Since the first element is always unique
-        // for (int i = 1; i < nums.length; i++) {
-        //     if (nums[i] != nums[i - 1]) {
-        //         nums[k] = nums[i];
-        //         k++;
-        //     }
-        // }
-        // return k;
-
-        int j = 1;
-        int n = a.length;
-        for (int i = 0; i < n - 1; i++) {
-            if (a[i] < a[i + 1]) {
-                a[j] = a[i + 1];
-                j++;
+        int k = 1; // Since the first element is always unique
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
             }
         }
-        return j;
+        return k;
+
+        // int j = 1;
+        // int n = a.length;
+        // for (int i = 0; i < n - 1; i++) {
+        //     if (a[i] < a[i + 1]) {
+        //         a[j] = a[i + 1];
+        //         j++;
+        //     }
+        // }
+        // return j;
     }
 }
 
