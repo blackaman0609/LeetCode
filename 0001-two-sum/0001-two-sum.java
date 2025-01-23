e0d9b60b46e982f701c3978ff3797b6fe7b95184
Aman Kumar <149137677+blackaman0609@users.noreply.github.com>
2025-01-23 18:33:46 +0530

    Time: 39 ms (40.41%), Space: 44.7 MB (86.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,26 +1,42 @@
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         // Create a HashMap to store the complement and its index
+//         HashMap<Integer, Integer> map = new HashMap<>();
+        
+//         // Iterate through the array
+//         for (int i = 0; i < nums.length; i++) {
+//             // Calculate the complement
+//             int complement = target - nums[i];
+            
+//             // Check if the complement exists in the map
+//             if (map.containsKey(complement)) {
+//                 // Return the indices of the two numbers
+//                 return new int[] { map.get(complement), i };
+//             }
+            
+//             // Store the current number and its index in the map
+//             map.put(nums[i], i);
+//         }
+        
+//         // If no solution is found, throw an exception (problem guarantees a solution)
+//         throw new IllegalArgumentException("No two sum solution");
+        
+//     }
+// }
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Create a HashMap to store the complement and its index
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        // Iterate through the array
-        for (int i = 0; i < nums.length; i++) {
-            // Calculate the complement
-            int complement = target - nums[i];
-            
-            // Check if the complement exists in the map
-            if (map.containsKey(complement)) {
-                // Return the indices of the two numbers
-                return new int[] { map.get(complement), i };
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) { 
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
             }
-            
-            // Store the current number and its index in the map
-            map.put(nums[i], i);
         }
-        
-        // If no solution is found, throw an exception (problem guarantees a solution)
-        throw new IllegalArgumentException("No two sum solution");
-        
+        return null; 
     }
 }
+
 
