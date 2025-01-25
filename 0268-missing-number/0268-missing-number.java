020ae3e2e832b28daac0aad3139ff0e90730d978
Aman Kumar <149137677+blackaman0609@users.noreply.github.com>
2025-01-26 00:02:36 +0530

    Time: 0 ms (100%), Space: 45.7 MB (13.14%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,28 +1,29 @@
-// class Solution {
-//     public int missingNumber(int[] nums) {
-//         int n = nums.length;
-//         int sum1 = n * (n + 1) / 2;
-//         int sum2 = 0;
-//         for (int i : nums)
-//         sum2 += i;
-//         return sum1 - sum2;
-//     }
-// }
-
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-    int hash[] = new int[n + 1];
-
-    for (int i = 0; i < n; i++) {
-      hash[nums[i]]++;
-    }
-
-    for (int i = 0; i < n + 1; i++) {
-      if (hash[i] == 0) {
-        return i;
-      }
-    }
-    return -1;
+        int sum1 = n * (n + 1) / 2;
+        int sum2 = 0;
+        for (int i : nums)
+        sum2 += i;
+        return sum1 - sum2;
     }
 }
+
+
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         int n = nums.length;
+//     int hash[] = new int[n + 1];
+
+//     for (int i = 0; i < n; i++) {
+//       hash[nums[i]]++;
+//     }
+
+//     for (int i = 0; i < n + 1; i++) {
+//       if (hash[i] == 0) {
+//         return i;
+//       }
+//     }
+//     return -1;
+//     }
+// }
