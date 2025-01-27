7cd5a763e9535d6f55ffe8de2224b2375e5623f0
Aman Kumar <149137677+blackaman0609@users.noreply.github.com>
2025-01-27 18:41:47 +0530

    Time: 11 ms (23.27%), Space: 45.4 MB (85.67%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,22 +1,41 @@
-
 class Solution {
     public int singleNumber(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if (arr[j] == arr[i] && i!=j) {
-                count++;
-                break;
-                }
-            }
-            if (count == 0) {
-                return arr[i];
+        Set<Integer> s = new HashSet<>();
+
+        for (int i : arr) {
+            // Add if not present, else remove if already present
+            if (s.contains(i)) {
+                s.remove(i);
+            } else {
+                s.add(i);
             }
         }
-        return -1;
+
+        // At this point, the HashSet will have only one element
+        // Convert the Set to an Array and return the single element
+        return s.iterator().next();
     }
 }
+
+
+// class Solution {
+//     public int singleNumber(int[] arr) {
+//         int n = arr.length;
+//         for (int i = 0; i < n; i++) {
+//             int count = 0;
+//             for (int j = 0; j < n; j++) {
+//                 if (arr[j] == arr[i] && i!=j) {
+//                 count++;
+//                 break;
+//                 }
+//             }
+//             if (count == 0) {
+//                 return arr[i];
+//             }
+//         }
+//         return -1;
+//     }
+// }
 
 
 
