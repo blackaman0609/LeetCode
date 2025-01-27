bd4271537c8e9063e65d0f39d3c71eca9b455249
Aman Kumar <149137677+blackaman0609@users.noreply.github.com>
2025-01-27 18:28:36 +0530

    Time: 213 ms (7.33%), Space: 46.3 MB (46.55%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -5,11 +5,12 @@ class Solution {
         for (int i = 0; i < n; i++) {
             int count = 0;
             for (int j = 0; j < n; j++) {
-                if (arr[j] == arr[i]) {
+                if (arr[j] == arr[i] && i!=j) {
                 count++;
+                break;
                 }
             }
-            if (count == 1) {
+            if (count == 0) {
                 return arr[i];
             }
         }
