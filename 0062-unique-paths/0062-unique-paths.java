ae814c9ccaa1030457b21137bd58aa786ee8f7b2
Aman Kumar <149137677+blackaman0609@users.noreply.github.com>
2025-06-12 11:19:03 +0530

    Time: 1 ms (7.96%), Space: 40.9 MB (17.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,25 +1,56 @@
-// Memoization
+// Tabulation
 
-import java.util.Arrays;
-
-class Solution {
-    public int solve(int i, int j, int[][] memo) {
-        if (i < 0 || j < 0) return 0;
-        if (i == 0 && j == 0) return 1;
-        if (memo[i][j] != -1) return memo[i][j];
-
-        memo[i][j] = solve(i - 1, j, memo) + solve(i, j - 1, memo);
-        return memo[i][j];
-    }
-
-    public int uniquePaths(int m, int n) {
-        int[][] memo = new int[m][n];
-        for (int[] row : memo) {
-            Arrays.fill(row, -1);
-        }
-        return solve(m - 1, n - 1, memo);
+class Solution
+{
+   
+    public int uniquePaths(int n, int m)//n=3 m=3
+    {
+        int[][] memo = new int[n+1][m+1]; 
+        int i,j; 
+        for(i=0;i<=n;i++)
+        {
+            for(j=0;j<=m;j++)
+            {
+               if(i==0 || j==0)
+               {
+                memo[i][j] = 0;
+               }
+               else if(i==1&&j==1)
+               {
+                memo[i][j]=1;
+               }
+               else 
+               {
+                memo[i][j] = memo[i-1][j] + memo[i][j-1];
+               }
+            }
+        } 
+        return memo[n][m];
     }
 }
+
+// Memoization
+
+// import java.util.Arrays;
+
+// class Solution {
+//     public int solve(int i, int j, int[][] memo) {
+//         if (i < 0 || j < 0) return 0;
+//         if (i == 0 && j == 0) return 1;
+//         if (memo[i][j] != -1) return memo[i][j];
+
+//         memo[i][j] = solve(i - 1, j, memo) + solve(i, j - 1, memo);
+//         return memo[i][j];
+//     }
+
+//     public int uniquePaths(int m, int n) {
+//         int[][] memo = new int[m][n];
+//         for (int[] row : memo) {
+//             Arrays.fill(row, -1);
+//         }
+//         return solve(m - 1, n - 1, memo);
+//     }
+// }
 
 
 // Recursion
