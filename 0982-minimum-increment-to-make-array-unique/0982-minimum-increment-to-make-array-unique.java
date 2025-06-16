71932b13cb90edd8f3b0ae2686345317dc95c548
Aman Kumar <149137677+blackaman0609@users.noreply.github.com>
2025-06-16 12:25:27 +0530

    Time: 38 ms (38.5%), Space: 57.7 MB (54.77%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.java
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.java
@@ -1,16 +1,37 @@
 class Solution {
     public int minIncrementForUnique(int[] nums) {
-        Arrays.sort(nums); // Sort the array
-        int count = 0;
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] <= nums[i - 1]) {
-                int increment = nums[i - 1] + 1 - nums[i];
-                nums[i] = nums[i - 1] + 1;
-                count += increment;
+        
+        Arrays.sort(nums);
+        int n=nums.length;
+        int count=0,i;
+        for(i=1;i<n;i++) {
+            if(nums[i-1]>=nums[i]) {
+                count=count+(nums[i-1]-nums[i]+1);
+                nums[i]=nums[i-1]+1;
             }
         }
 
         return count;
     }
 }
+
+
+
+
+// class Solution {
+//     public int minIncrementForUnique(int[] nums) {
+//         HashSet<Integer> set = new HashSet<>();
+//         int count = 0;
+
+//         for (int i = 0; i < nums.length; i++) {
+//             int current = nums[i];
+//             while (set.contains(current)) {
+//                 current++;
+//                 count++;
+//             }
+//             set.add(current);
+//         }
+
+//         return count;
+//     }
+// }
