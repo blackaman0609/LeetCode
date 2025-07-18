cbe5ebd80f79f7d88177f001e9896589e524389c
Aman Kumar <149137677+blackaman0609@users.noreply.github.com>
2025-07-18 17:20:06 +0530

    Time: 4 ms (77.38%), Space: 44.4 MB (56.06%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n = s.length();
+
+        char array1[] = s.toCharArray();
+        Arrays.sort(array1);
+        s = new String(array1);
+
+        char array2[] = t.toCharArray();
+        Arrays.sort(array2);
+        t = new String(array2);
+
+        return Arrays.equals(array1, array2);
+
+    }
+}
